44cfde1c740e504ca1ba240decaef34d0f600817
Marcos <marcus.furtuna@memora.com.br>
2025-11-17 22:10:32 -0300

    ajuste no front

--- frontend/src/app/pacientes/pacientes.routes.ts
+++ frontend/src/app/pacientes/pacientes.routes.ts
@@ -5,8 +5,8 @@ import { PacienteFormComponent } from './paciente-form.component';
 import { RoleGuard } from '../core/role.guard';
 
 export const PACIENTES_ROUTES: Routes = [
-  { path: '', loadComponent: () => import('./paciente-list.component').then(m => PacienteListComponent) },
-  { path: 'novo', canActivate: [RoleGuard], data: { roles: ['ADMIN','ATENDENTE'] }, loadComponent: () => import('./paciente-form.component').then(m => PacienteFormComponent) },
-  { path: ':id/editar', canActivate: [RoleGuard], data: { roles: ['ADMIN','ATENDENTE'] }, loadComponent: () => import('./paciente-edit.component').then(m => PacienteEditComponent) },
-  { path: ':id', loadComponent: () => import('./paciente-detail.component').then(m => PacienteDetailComponent) }
+  { path: '', loadComponent: () => import('./paciente-list.component').then(m => m.PacienteListComponent) },
+  { path: 'novo', canActivate: [RoleGuard], data: { roles: ['ADMIN','ATENDENTE'] }, loadComponent: () => import('./paciente-form.component').then(m => m.PacienteFormComponent) },
+  { path: ':id/editar', canActivate: [RoleGuard], data: { roles: ['ADMIN','ATENDENTE'] }, loadComponent: () => import('./paciente-edit.component').then(m => m.PacienteEditComponent) },
+  { path: ':id', loadComponent: () => import('./paciente-detail.component').then(m => m.PacienteDetailComponent) }
 ];
